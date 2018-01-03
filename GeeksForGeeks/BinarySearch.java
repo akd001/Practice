719cf7266aee1ab90ec624532505658df30bcd18
akd001 <001.amby@gmail.com>
2018-01-04 02:53:34 +0530

    binary search

--- GeeksForGeeks/BinarySearch.java
+++ GeeksForGeeks/BinarySearch.java
@@ -1,39 +1,46 @@
 class BinarySearch
 {
-    // Returns index of x if it is present in arr[l..r], else
-    // return -1
-    int binarySearch(int arr[], int l, int r, int x)
-    {
-        if (r>=l)
-        {
+    int binarySearchRecursive(int arr[], int l, int r, int x) {
+        if (r>=l) {
             int mid = l + (r - l)/2;
  
-            // If the element is present at the middle itself
             if (arr[mid] == x)
                return mid;
  
-            // If element is smaller than mid, then it can only
-            // be present in left subarray
             if (arr[mid] > x)
-               return binarySearch(arr, l, mid-1, x);
+               return binarySearchRecursive(arr, l, mid-1, x);
  
-            // Else the element can only be present in right
-            // subarray
-            return binarySearch(arr, mid+1, r, x);
+            return binarySearchRecursive(arr, mid+1, r, x);
         }
  
-        // We reach here when element is not present in array
+        return -1;
+    }
+
+    int binarySearchIterative(int arr[], int x) {
+        int l = 0;
+        int r = arr.length-1;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+
+            if (arr[mid] == x)
+                return mid;
+                
+            if (arr[mid]>x)
+                r = mid-1;
+            else
+                l = mid+1;
+        }
         return -1;
     }
  
-    // Driver method to test above
     public static void main(String args[])
     {
         BinarySearch ob = new BinarySearch();
         int arr[] = {2,3,4,10,40};
         int n = arr.length;
         int x = 10;
-        int result = ob.binarySearch(arr,0,n-1,x);
+        // int result = ob.binarySearchRecursive(arr,0,n-1,x);
+        int result = ob.binarySearchIterative(arr,x);
         if (result == -1)
             System.out.println("Element not present");
         else
