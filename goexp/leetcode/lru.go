8436bbe0062a01ddb4264f6b522c8b25d2c089a1
ambar <ambar@pop-os.localdomain>
2023-07-20 05:56:31 +0530

    added leetcode

---
+++ goexp/leetcode/lru.go
@@ -0,0 +1,81 @@
+package leetcode
+
+// https://leetcode.com/problems/lru-cache
+
+import "fmt"
+
+
+type CacheNode struct {
+	key int
+	value int
+	prev, next *CacheNode
+}
+
+
+type LRUCache struct {
+    capacity int
+	head, tail *CacheNode
+	cache map[int]*CacheNode
+}
+
+
+func Constructor(capacity int) LRUCache {
+	temp := $CacheNode{}
+
+	return LRUCache{
+		capacity: capacity, 
+		head: temp,
+		tail: temp,
+		cache: make(map[int]*CacheNode)
+	}
+    
+}
+
+func RemoveCacheNode(lru LRUCache, key int) LRUCache {
+	nodeUpForDeletion := lru.cache[key]
+	if (nodeUpForDeletion.prev) nodeUpForDeletion.prev.next = nodeUpForDeletion.next
+	if (nodeUpForDeletion.next) nodeUpForDeletion.next.prev = nodeUpForDeletion.prev
+	delete(lru.cache, key)
+	return lru
+}
+
+
+func (this *LRUCache) Get(key int) int {
+    
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+	node := &CacheNode{
+		key: key
+		value: value
+	}
+
+	// if number of elements is less than capacity
+	if (len(this.cache) < this.capacity) {
+		// if key already exists, remove existing node
+		if (this.cache[key]) {
+			RemoveCacheNode(this, key)
+		}
+		// add new node at head
+		node.next = this.head
+		this.head = node
+		// update cache value
+		this.cache[key] = node
+		// if this is the only node present, tail points to itself
+		if (len(this.cache)==1) this.tail = node
+	} 
+	// if number of elements is equal to (or more than - which is improbable if this logic is correct)
+	else {
+		// remove tail
+	}
+
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
---
+++ goexp/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+import "goexp/leetcode"
+
+func main() {
+	fmt.Println("Hello")
+
+	obj := Constructor(capacity);
+	param_1 := obj.Get(key);
+	obj.Put(key,value);
+}
